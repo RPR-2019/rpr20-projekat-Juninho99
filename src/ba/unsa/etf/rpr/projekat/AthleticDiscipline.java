7c79e67063928b7f2aa57597b927ea327e2f4e77
Armin Žunić <azunic1@etf.unsa.ba>
2020-12-26 02:08:55 +0100

    Dodana klasa AthleticDiscipline u kojoj će se kao atributi nalaziti sve instance enum klasa koje će ustvari biti podjele atletskih disciplina.

---
+++ src/ba/unsa/etf/rpr/projekat/AthleticDiscipline.java
@@ -0,0 +1,17 @@
+package ba.unsa.etf.rpr.projekat;
+
+public class AthleticDiscipline {
+    private SprintsHurdlesRelays sprintsHurdlesRelays;
+
+    public AthleticDiscipline(SprintsHurdlesRelays sprintsHurdlesRelays) {
+        this.sprintsHurdlesRelays = sprintsHurdlesRelays;
+    }
+
+    public SprintsHurdlesRelays getSprintsHurdlesRelays() {
+        return sprintsHurdlesRelays;
+    }
+
+    public void setSprintsHurdlesRelays(SprintsHurdlesRelays sprintsHurdlesRelays) {
+        this.sprintsHurdlesRelays = sprintsHurdlesRelays;
+    }
+}
